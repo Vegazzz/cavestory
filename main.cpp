ca7f9d127d33f31697cd83fd3710c7640934c129
Vegazzz <silvershadowtv@gmail.com>
2017-01-18 18:46:38 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*Main.cpp
+The entry point of the program
+*/
+
+#include "game.h"
+#include <Windows.h>
+
+int main(int argc, char* argv[])
+{
+	//FreeConsole();  //Closes the console!
+	Game game;
+	return 0;
+}
---
+++ sprites.h
@@ -0,0 +1,32 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <SDL.h>
+
+#include <string>
+
+#include "globals.h"
+
+class Graphics;
+
+/*Sprite Class
+	Holds all information for individual sprites
+*/
+class Sprite
+{
+public:
+	Sprite();
+	Sprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width, int height, float posX, float posY);
+	virtual ~Sprite();
+	virtual void update();
+	void draw(Graphics &graphics, int x, int y);
+
+protected:
+	SDL_Rect _sourceRect;
+	SDL_Texture* _spriteSheet;
+	float _x, _y;
+
+private:
+};
+
+#endif
